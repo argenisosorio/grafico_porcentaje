2f9e2beda9366d0fa1efd35e6600ae557255b5dc
aosorio <argenisosorio580@hotmail.com>
2018-09-25 16:57:10 -0400

    Agregando nuevos campos en el modelo de datos de los proyectos

--- registro/forms.py
+++ registro/forms.py
@@ -16,6 +16,18 @@ class ProyectoForm(forms.ModelForm):
         'required': 'True',
     }), required = True)
 
+    ano_ejecucion = forms.CharField(label='Año de ejecución', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+        'required': 'True',
+    }), required = True)
+
+    fecha_entrega_proyecto = forms.CharField(label='Fecha de entrega', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+        'required': 'True',
+    }), required = True)
+
     nombre_caravisible = forms.CharField(label='Nombre del Cara Visible', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
@@ -23,6 +35,12 @@ class ProyectoForm(forms.ModelForm):
     }), required = True)
 
     nombre_director = forms.CharField(label='Nombre del Director', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+        'required': 'True',
+    }), required = True)
+
+    integrantes_equipo = forms.CharField(label='Integrantes del equipo', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
         'required': 'True',
@@ -79,6 +97,107 @@ class ProyectoForm(forms.ModelForm):
         'style': 'min-width: 0; width: 100%; display: inline;',
     }), required = False)
 
+    nombre_act_11 = forms.CharField(label='Nombre de la Actividad 11', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    nombre_act_12 = forms.CharField(label='Nombre de la Actividad 12', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    nombre_act_13 = forms.CharField(label='Nombre de la Actividad 13', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    nombre_act_14 = forms.CharField(label='Nombre de la Actividad 14', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    nombre_act_15 = forms.CharField(label='Nombre de la Actividad 15', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    # Nombres de las actividades del proyecto.
+    resp_act_1 = forms.CharField(label='Responsable de la Actividad 1', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_2 = forms.CharField(label='Responsable de la Actividad 2', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_3 = forms.CharField(label='Responsable de la Actividad 3', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_4 = forms.CharField(label='Responsable de la Actividad 4', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_5 = forms.CharField(label='Responsable de la Actividad 5', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_6 = forms.CharField(label='Responsable de la Actividad 6', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_7 = forms.CharField(label='Responsable de la Actividad 7', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_8 = forms.CharField(label='Responsable de la Actividad 8', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_9 = forms.CharField(label='Responsable de la Actividad 9', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_10 = forms.CharField(label='Responsable de la Actividad 10', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_11 = forms.CharField(label='Responsable de la Actividad 11', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_12 = forms.CharField(label='Responsable de la Actividad 12', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_13 = forms.CharField(label='Responsable de la Actividad 13', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_14 = forms.CharField(label='Responsable de la Actividad 14', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    resp_act_15 = forms.CharField(label='Responsable de la Actividad 15', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
     # Fechas de entrega de las actividades del proyecto.
     fecha_entrega_act_1 = forms.CharField(label='Fecha de entrega de la Actividad 1', widget=TextInput(attrs={
         'class':'form-control input-md',
@@ -126,6 +245,31 @@ class ProyectoForm(forms.ModelForm):
     }), required = False)
 
     fecha_entrega_act_10 = forms.CharField(label='Fecha de entrega de la Actividad 10', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    fecha_entrega_act_11 = forms.CharField(label='Fecha de entrega de la Actividad 11', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    fecha_entrega_act_12 = forms.CharField(label='Fecha de entrega de la Actividad 12', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    fecha_entrega_act_13 = forms.CharField(label='Fecha de entrega de la Actividad 13', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    fecha_entrega_act_14 = forms.CharField(label='Fecha de entrega de la Actividad 14', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    fecha_entrega_act_15 = forms.CharField(label='Fecha de entrega de la Actividad 15', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
     }), required = False)
@@ -181,55 +325,120 @@ class ProyectoForm(forms.ModelForm):
         'style': 'min-width: 0; width: 100%; display: inline;',
     }), required = False)
 
+    porc_act_11 = forms.CharField(label='Porcentaje de la Actividad 11', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    porc_act_12 = forms.CharField(label='Porcentaje de la Actividad 12', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    porc_act_13 = forms.CharField(label='Porcentaje de la Actividad 13', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    porc_act_14 = forms.CharField(label='Porcentaje de la Actividad 14', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
+    porc_act_15 = forms.CharField(label='Porcentaje de la Actividad 15', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+    }), required = False)
+
     # Porcentaje de avance de las actividades del proyecto.
     porc_avan_act_1 = forms.CharField(label='Porcentaje de avance la Actividad 1', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_2 = forms.CharField(label='Porcentaje de avance la Actividad 2', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_3 = forms.CharField(label='Porcentaje de avance la Actividad 3', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_4 = forms.CharField(label='Porcentaje de avance la Actividad 4', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_5 = forms.CharField(label='Porcentaje de avance la Actividad 5', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_6 = forms.CharField(label='Porcentaje de avance la Actividad 6', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_7 = forms.CharField(label='Porcentaje de avance la Actividad 7', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_8 = forms.CharField(label='Porcentaje de avance la Actividad 8', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_9 = forms.CharField(label='Porcentaje de avance la Actividad 9', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     porc_avan_act_10 = forms.CharField(label='Porcentaje de avance la Actividad 10', widget=TextInput(attrs={
         'class':'form-control input-md',
         'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
+    }), required = False)
+
+    porc_avan_act_11 = forms.CharField(label='Porcentaje de avance la Actividad 11', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
+    }), required = False)
+
+    porc_avan_act_12 = forms.CharField(label='Porcentaje de avance la Actividad 12', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
+    }), required = False)
+
+    porc_avan_act_13 = forms.CharField(label='Porcentaje de avance la Actividad 13', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
+    }), required = False)
+
+    porc_avan_act_14 = forms.CharField(label='Porcentaje de avance la Actividad 14', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
+    }), required = False)
+
+    porc_avan_act_15 = forms.CharField(label='Porcentaje de avance la Actividad 15', widget=TextInput(attrs={
+        'class':'form-control input-md',
+        'style': 'min-width: 0; width: 100%; display: inline;',
+        'value': '0',
     }), required = False)
 
     class Meta:
--- registro/models.py
+++ registro/models.py
@@ -8,8 +8,11 @@ class Proyecto(models.Model):
     Modelo que contiene los campos de un Proyecto.
     """
     nombre_proyecto = models.CharField(max_length=4000, blank=True,null=True)
+    ano_ejecucion = models.CharField(max_length=4000, blank=True,null=True)
+    fecha_entrega_proyecto = models.CharField(max_length=4000, blank=True,null=True)
     nombre_caravisible = models.CharField(max_length=4000, blank=True,null=True)
     nombre_director = models.CharField(max_length=4000, blank=True,null=True)
+    integrantes_equipo = models.CharField(max_length=4000, blank=True,null=True)
     # Nombres de las actividades del proyecto.
     nombre_act_1 = models.CharField(max_length=4000, blank=True,null=True)
     nombre_act_2 = models.CharField(max_length=4000, blank=True,null=True)
@@ -21,6 +24,27 @@ class Proyecto(models.Model):
     nombre_act_8 = models.CharField(max_length=4000, blank=True,null=True)
     nombre_act_9 = models.CharField(max_length=4000, blank=True,null=True)
     nombre_act_10 = models.CharField(max_length=4000, blank=True,null=True)
+    nombre_act_11 = models.CharField(max_length=4000, blank=True,null=True)
+    nombre_act_12 = models.CharField(max_length=4000, blank=True,null=True)
+    nombre_act_13 = models.CharField(max_length=4000, blank=True,null=True)
+    nombre_act_14 = models.CharField(max_length=4000, blank=True,null=True)
+    nombre_act_15 = models.CharField(max_length=4000, blank=True,null=True)
+    # Nombres de las actividades del proyecto.
+    resp_act_1 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_2 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_3 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_4 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_5 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_6 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_7 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_8 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_9 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_10 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_11 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_12 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_13 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_14 = models.CharField(max_length=4000, blank=True,null=True)
+    resp_act_15 = models.CharField(max_length=4000, blank=True,null=True)
     # Fechas de entrega de las actividades del proyecto.
     fecha_entrega_act_1 = models.CharField(max_length=100, blank=True,null=True)
     fecha_entrega_act_2 = models.CharField(max_length=100, blank=True,null=True)
@@ -32,6 +56,11 @@ class Proyecto(models.Model):
     fecha_entrega_act_8 = models.CharField(max_length=100, blank=True,null=True)
     fecha_entrega_act_9 = models.CharField(max_length=100, blank=True,null=True)
     fecha_entrega_act_10 = models.CharField(max_length=100, blank=True,null=True)
+    fecha_entrega_act_11 = models.CharField(max_length=100, blank=True,null=True)
+    fecha_entrega_act_12 = models.CharField(max_length=100, blank=True,null=True)
+    fecha_entrega_act_13 = models.CharField(max_length=100, blank=True,null=True)
+    fecha_entrega_act_14 = models.CharField(max_length=100, blank=True,null=True)
+    fecha_entrega_act_15 = models.CharField(max_length=100, blank=True,null=True)
     # Porcentaje de las actividades del proyecto.
     porc_act_1 = models.CharField(max_length=100, blank=True,null=True)
     porc_act_2 = models.CharField(max_length=100, blank=True,null=True)
@@ -43,6 +72,11 @@ class Proyecto(models.Model):
     porc_act_8 = models.CharField(max_length=100, blank=True,null=True)
     porc_act_9 = models.CharField(max_length=100, blank=True,null=True)
     porc_act_10 = models.CharField(max_length=100, blank=True,null=True)
+    porc_act_11 = models.CharField(max_length=100, blank=True,null=True)
+    porc_act_12 = models.CharField(max_length=100, blank=True,null=True)
+    porc_act_13 = models.CharField(max_length=100, blank=True,null=True)
+    porc_act_14 = models.CharField(max_length=100, blank=True,null=True)
+    porc_act_15 = models.CharField(max_length=100, blank=True,null=True)
     # Porcentaje de avance de las actividades del proyecto.
     porc_avan_act_1 = models.IntegerField()
     porc_avan_act_2 = models.IntegerField()
@@ -54,9 +88,14 @@ class Proyecto(models.Model):
     porc_avan_act_8 = models.IntegerField()
     porc_avan_act_9 = models.IntegerField()
     porc_avan_act_10 = models.IntegerField()
+    porc_avan_act_11 = models.IntegerField()
+    porc_avan_act_12 = models.IntegerField()
+    porc_avan_act_13 = models.IntegerField()
+    porc_avan_act_14 = models.IntegerField()
+    porc_avan_act_15 = models.IntegerField()
 
     def sumatoria_por_avan(self):
-        sumatoria_por_avan = self.porc_avan_act_1+self.porc_avan_act_2+self.porc_avan_act_3+self.porc_avan_act_4+self.porc_avan_act_5+self.porc_avan_act_6+self.porc_avan_act_7+self.porc_avan_act_8+self.porc_avan_act_9+self.porc_avan_act_10
+        sumatoria_por_avan = self.porc_avan_act_1+self.porc_avan_act_2+self.porc_avan_act_3+self.porc_avan_act_4+self.porc_avan_act_5+self.porc_avan_act_6+self.porc_avan_act_7+self.porc_avan_act_8+self.porc_avan_act_9+self.porc_avan_act_10+self.porc_avan_act_11+self.porc_avan_act_12+self.porc_avan_act_13+self.porc_avan_act_14+self.porc_avan_act_15
         return sumatoria_por_avan
 
     def __unicode__(self):
